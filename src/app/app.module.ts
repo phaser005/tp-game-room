a643bd3e240ad3339ec7ff1ae7aceeaf4a838b4a
elias <elias.m.alvarez@gmail.com>
2021-04-01 02:41:02 -0300

    new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { WhoAmIComponent } from './pages/who-am-i/who-am-i.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    WhoAmIComponent
   ],
   imports: [
     BrowserModule,
